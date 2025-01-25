3a1f1d2a3f72db65fc9ff6f5bf5ea6d8a0d69ffb
Nefta11 <neftaliarturohernandez@gmail.com>
2025-01-25 17:19:59 -0600

    Añade íconos a los elementos de navegación en NavComponent para mejorar la visualización en el menú

--- src/components/generals/navComponent.jsx
+++ src/components/generals/navComponent.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
-import { FaBars } from 'react-icons/fa';
+import { FaBars, FaHome, FaSuitcase, FaInfoCircle, FaBlog, FaImages, FaEnvelope } from 'react-icons/fa';
 import colors from '../../utils/colors';
 import logo from '../../assets/images/goodTravel.png';
 import './navComponent.css';
@@ -23,22 +23,22 @@ const NavComponent = () => {
                 </div>
                 <ul className={`nav-list ${menuOpen ? 'open' : ''}`}>
                     <li className="nav-item">
-                        <Link to="/">Inicio</Link>
+                        {menuOpen && <FaHome />} <Link to="/">Inicio</Link>
                     </li>
                     <li className="nav-item">
-                        <Link to="/experiencias">Experiencias</Link>
+                        {menuOpen && <FaSuitcase />} <Link to="/experiencias">Experiencias</Link>
                     </li>
                     <li className="nav-item">
-                        <Link to="/nosotros">Nosotros</Link>
+                        {menuOpen && <FaInfoCircle />} <Link to="/nosotros">Nosotros</Link>
                     </li>
                     <li className="nav-item">
-                        <Link to="/blog">Blog</Link>
+                        {menuOpen && <FaBlog />} <Link to="/blog">Blog</Link>
                     </li>
                     <li className="nav-item">
-                        <Link to="/galeria">Galeria</Link>
+                        {menuOpen && <FaImages />} <Link to="/galeria">Galeria</Link>
                     </li>
                     <li className="nav-item">
-                        <Link to="/contacto">Contacto</Link>
+                        {menuOpen && <FaEnvelope />} <Link to="/contacto">Contacto</Link>
                     </li>
                 </ul>
             </div>
