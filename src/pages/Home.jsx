2e205f5d14bc018d676f7147746f0bb14d912692
Nefta11 <neftaliarturohernandez@gmail.com>
2025-01-27 23:02:15 -0600

    Mueve el componente WhatsAppIcon a una nueva ubicación y restaura su estilo CSS

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import NavComponent from '../components/home/nav/navComponent';
 import CarouselComponent from "../components/home/carousel/carouselComponent";
-import WhatsAppIcon from "../components/WhatsAppIcon";
+import WhatsAppIcon from "../components/generals/whatsAppComponent/WhatsAppIcon";
 
 export default function Home() {
   return (
