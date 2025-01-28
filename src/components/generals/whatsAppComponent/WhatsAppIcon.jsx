e105e8b9b06019a4bbb0e759834c05b7b5fdb6f4
Nefta11 <neftaliarturohernandez@gmail.com>
2025-01-28 16:30:29 -0600

    Actualiza el enlace del componente WhatsAppIcon para usar el esquema de URL de WhatsApp

--- src/components/generals/whatsAppComponent/WhatsAppIcon.jsx
+++ src/components/generals/whatsAppComponent/WhatsAppIcon.jsx
@@ -4,7 +4,7 @@ import './WhatsAppIcon.css'; // Importa el archivo CSS
 const WhatsAppIcon = () => {
     return (
         <a
-            href="https://wa.me/5217716075321"
+            href="whatsapp://send?phone=5217716075321"
             target="_blank"
             rel="noopener noreferrer"
             className="floating-button" // Clases CSS
