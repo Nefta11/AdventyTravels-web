f73c273714ffc1a0589c866d6e96563a911a915a
Nefta11 <neftaliarturohernandez@gmail.com>
2025-02-01 17:42:34 -0600

    Añade funcionalidad de pestañas activas en el componente TourSearch y mejora los estilos con transiciones

--- src/components/home/searchF/TourSearch.jsx
+++ src/components/home/searchF/TourSearch.jsx
@@ -1,19 +1,25 @@
 import "./TourSearch.css";
 import { useTranslation } from 'react-i18next';
 import { FaPlane, FaHotel, FaUmbrellaBeach, FaShieldAlt, FaPlaneDeparture } from 'react-icons/fa';
+import { useState } from 'react';
 
 const TourSearch = () => {
     const { t } = useTranslation();
+    const [activeTab, setActiveTab] = useState('tours');
+
+    const handleTabClick = (tab) => {
+        setActiveTab(tab);
+    };
 
     return (
         <div className="tour-search-container">
             <div className="tour-search-box">
                 <div className="tabs">
-                    <span className="tab active"><FaUmbrellaBeach /> {t('tourSearch.tours')}</span>
-                    <span className="tab"><FaPlane /> {t('tourSearch.flights')}</span>
-                    <span className="tab"><FaShieldAlt /> {t('tourSearch.insurance')}</span>
-                    <span className="tab"><FaHotel /> {t('tourSearch.hotel')}</span>
-                    <span className="tab"><FaPlaneDeparture /> {t('tourSearch.hotels')}</span>
+                    <span className={`tab ${activeTab === 'tours' ? 'active' : ''}`} onClick={() => handleTabClick('tours')}><FaUmbrellaBeach /> {t('tourSearch.tours')}</span>
+                    <span className={`tab ${activeTab === 'flights' ? 'active' : ''}`} onClick={() => handleTabClick('flights')}><FaPlane /> {t('tourSearch.flights')}</span>
+                    <span className={`tab ${activeTab === 'insurance' ? 'active' : ''}`} onClick={() => handleTabClick('insurance')}><FaShieldAlt /> {t('tourSearch.insurance')}</span>
+                    <span className={`tab ${activeTab === 'hotel' ? 'active' : ''}`} onClick={() => handleTabClick('hotel')}><FaHotel /> {t('tourSearch.hotel')}</span>
+                    <span className={`tab ${activeTab === 'hotels' ? 'active' : ''}`} onClick={() => handleTabClick('hotels')}><FaPlaneDeparture /> {t('tourSearch.hotels')}</span>
                 </div>
                 <h2>{t('tourSearch.findTour')}</h2>
                 <div className="search-fields">
