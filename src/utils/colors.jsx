9709499f97625258f6d0afbb5176db37d95fda9d
Nefta11 <neftaliarturohernandez@gmail.com>
2025-02-05 19:34:06 -0600

    Actualiza los colores en navComponent.css y colors.jsx para mejorar la presentación visual de la navegación.

--- src/utils/colors.jsx
+++ src/utils/colors.jsx
@@ -1,9 +1,8 @@
 const colors = {
-    color1: '#F2F2F2',
-    color2: '#F29863',
-    color3: '#ACD6CC',
-    color4: '#2D2C59',
-    color5: '#D9305D',
+    color1: "#F4F4F4",
+    color2: "#7DC1BD",
+    color3: " #E49F30",
+    color4: "#24455D"
 };
 
 export default colors;
