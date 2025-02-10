74c704d9ba7205bddf2f76c7a70c2ae21cd750f4
Nefta11 <neftaliarturohernandez@gmail.com>
2025-02-09 20:34:51 -0600

    Agrega estilos responsivos al selector de idioma y ajusta su posición en la navegación para mejorar la experiencia en dispositivos móviles.

--- src/components/home/nav/navComponent.jsx
+++ src/components/home/nav/navComponent.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { FaBars, FaHome, FaSuitcase, FaInfoCircle, FaBlog, FaImages, FaEnvelope } from 'react-icons/fa';
 import { useTranslation } from 'react-i18next';
 import colors from '../../../utils/colors';
@@ -10,10 +10,20 @@ import './navComponent.css';
 const NavComponent = () => {
     const { t } = useTranslation();
     const [menuOpen, setMenuOpen] = useState(false);
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= 800);
 
     const toggleMenu = () => {
         setMenuOpen(!menuOpen);
     };
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 800);
+        };
+
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
 
     return (
         <nav className="nav-container fixed-nav" style={{ backgroundColor: colors.color1 }}>
@@ -25,9 +35,8 @@ const NavComponent = () => {
                     </div>
                     {/* Logo de la aplicación */}
                     <img src={logo} alt="logo app" className="nav-logo" />
-
-                    {/* Selector de idioma al final */}
-                    <LanguageSelector className="language-selector-desktop" />
+                    {/* Selector de idioma fuera de la lista en pantallas pequeñas */}
+                    {isMobile && <LanguageSelector className="language-selector-mobile" />}
                 </div>
 
                 {/* Lista de navegación */}
@@ -38,6 +47,8 @@ const NavComponent = () => {
                     <NavItem to="/blog" icon={FaBlog} label={t('blog')} menuOpen={menuOpen} />
                     <NavItem to="/galeria" icon={FaImages} label={t('gallery')} menuOpen={menuOpen} />
                     <NavItem to="/contacto" icon={FaEnvelope} label={t('contact')} menuOpen={menuOpen} />
+                    {/* Selector de idioma dentro de la lista en pantallas grandes */}
+                    {!isMobile && <LanguageSelector className="language-selector-desktop" />}
                 </ul>
             </div>
         </nav>
