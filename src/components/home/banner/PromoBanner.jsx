4f0acb63d394f7e74f4cd3407a4c0deb36341eb8
Nefta11 <neftaliarturohernandez@gmail.com>
2025-02-15 12:26:03 -0600

    Actualiza la imagen del banner en PromoBanner.jsx y elimina la imagen obsoleta banner1.webp

--- src/components/home/banner/PromoBanner.jsx
+++ src/components/home/banner/PromoBanner.jsx
@@ -1,5 +1,5 @@
 import "./PromoBanner.css";
-import bannerImage from "../../../assets/images/banner1.webp"; 
+import bannerImage from "../../../assets/images/banner.webp"; 
 
 const PromoBanner = () => {
     return (
