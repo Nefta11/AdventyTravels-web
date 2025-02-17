c13fb8b798ab679177156a5c5bbe99d777213187
Nefta11 <neftaliarturohernandez@gmail.com>
2025-02-17 17:13:57 -0600

    Especifica el directorio de archivos estáticos en la configuración de Vite

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  publicDir: 'public' // Añadir esta línea para especificar el directorio de archivos estáticos
 })
