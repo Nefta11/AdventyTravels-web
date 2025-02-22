9863d760cf82e27a5a9f13d7698639cce03ceb0a
Nefta11 <neftaliarturohernandez@gmail.com>
2025-02-22 17:48:55 -0600

    Actualiza las rutas de las imágenes en el componente UnderConstruction para apuntar a la rama principal.

--- src/components/underConstruction/UnderConstruction.jsx
+++ src/components/underConstruction/UnderConstruction.jsx
@@ -7,11 +7,11 @@ const UnderConstruction = ({ pageName }) => {
             <h2 className="construction-title">
                 La sección de {pageName} está en construcción
             </h2>
-            <img src="https://raw.githubusercontent.com/Nefta11/GoodTravel-web/refs/heads/update/web/src/assets/images/LOGOTIPO_OFICIAL.webp" alt="En Construcción" className="construction-logo" />
+            <img src="https://raw.githubusercontent.com/Nefta11/GoodTravel-web/refs/heads/main/src/assets/images/LOGOTIPO_OFICIAL.webp" alt="En Construcción" className="construction-logo" />
             <p className="construction-text">
                 Estamos trabajando para ofrecerte la mejor experiencia. ¡Vuelve pronto!
             </p>
-            <img src="https://raw.githubusercontent.com/Nefta11/GoodTravel-web/refs/heads/update/web/src/assets/page-construction.gif" alt="Cargando..." className="construction-gif" />
+            <img src="https://raw.githubusercontent.com/Nefta11/GoodTravel-web/refs/heads/main/src/assets/page-construction.gif" alt="Cargando..." className="construction-gif" />
         </div>
     );
 };
