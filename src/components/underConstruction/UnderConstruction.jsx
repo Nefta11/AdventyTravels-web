d25bfc31e19cf8969ada4245b801c8d2183528cc
Nefta11 <neftaliarturohernandez@gmail.com>
2025-02-22 17:02:45 -0600

    Mejora la presentación del componente UnderConstruction: ajusta el tamaño de la imagen, aumenta el tamaño de la fuente del título y añade estilos responsivos.

--- src/components/underConstruction/UnderConstruction.jsx
+++ src/components/underConstruction/UnderConstruction.jsx
@@ -3,10 +3,10 @@ import './underConstruction.css';
 const UnderConstruction = () => {
     return (
         <div className="under-construction">
-            <img src="https://raw.githubusercontent.com/Nefta11/GoodTravel-web/refs/heads/update/web/src/assets/images/LOGOTIPO_OFICIAL.webp" alt="En Construcción" className="construction-logo" />
             <h2 className="construction-title">
                 Esta página está en construcción
             </h2>
+            <img src="https://raw.githubusercontent.com/Nefta11/GoodTravel-web/refs/heads/update/web/src/assets/images/LOGOTIPO_OFICIAL.webp" alt="En Construcción" className="construction-logo" />
             <p className="construction-text">
                 Estamos trabajando para ofrecerte la mejor experiencia. ¡Vuelve pronto!
             </p>
