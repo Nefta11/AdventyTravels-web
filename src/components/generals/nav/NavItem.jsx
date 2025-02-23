010ab7143bd8f5b008f70b37be81c9411b3bc4ee
Nefta11 <neftaliarturohernandez@gmail.com>
2025-02-23 12:22:42 -0600

    Mejora la usabilidad del componente NavItem al eliminar la apertura del submenú al pasar el cursor y ajustar enlaces para dispositivos móviles.

--- src/components/generals/nav/NavItem.jsx
+++ src/components/generals/nav/NavItem.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { FaWhatsapp, FaFacebookMessenger, FaPhone } from 'react-icons/fa';
@@ -6,10 +6,22 @@ import "./NavItem.css";
 
 const NavItem = ({ to, icon: Icon, label, menuOpen, submenu }) => {
     const [submenuOpen, setSubmenuOpen] = useState(false);
+    const [isMobile, setIsMobile] = useState(false);
 
     const handleSubmenuToggle = () => {
         setSubmenuOpen(!submenuOpen);
     };
+
+    useEffect(() => {
+        if (!menuOpen) {
+            setSubmenuOpen(false);
+        }
+    }, [menuOpen]);
+
+    useEffect(() => {
+        const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+        setIsMobile(/android|iPad|iPhone|iPod/.test(userAgent.toLowerCase()));
+    }, []);
 
     return (
         <li className="nav-item">
@@ -28,7 +40,7 @@ const NavItem = ({ to, icon: Icon, label, menuOpen, submenu }) => {
                         </a>
                     </li>
                     <li>
-                        <a href="https://www.facebook.com/messages/t/108316027661637?locale=es_LA" target="_blank" rel="noopener noreferrer">
+                        <a href={isMobile ? "fb-messenger://user/108316027661637" : "https://www.facebook.com/messages/t/108316027661637?locale=es_LA"} target="_blank" rel="noopener noreferrer">
                             Via Messenger <FaFacebookMessenger className="submenu-icon" />
                         </a>
                     </li>
