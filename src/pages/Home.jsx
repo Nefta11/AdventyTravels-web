b19a7a758e48f6ec0d76b93000e582093261eeb8
Nefta11 <neftaliarturohernandez@gmail.com>
2025-02-23 13:15:56 -0600

    Añade animación de cambio de color al borde del componente de navegación y ajusta la animación de entrada/salida en la página de inicio

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -24,9 +24,9 @@ export default function Home() {
 
   return (
     <motion.div
-      initial={{ scale: 0 }}
-      animate={{ scale: 1 }}
-      exit={{ scale: 0 }}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
     >
       {showForm && <TravelForm />}
       <NavComponent />
