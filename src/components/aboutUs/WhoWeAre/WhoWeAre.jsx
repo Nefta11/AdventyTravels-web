8cf35ee8ee8cfbdd2f26febfd2740f6810446190
Nefta11 <neftaliarturohernandez@gmail.com>
2025-02-23 15:25:42 -0600

    Añade traducciones al componente "Quiénes somos" para mejorar la accesibilidad y la experiencia del usuario en múltiples idiomas.

--- src/components/aboutUs/WhoWeAre/WhoWeAre.jsx
+++ src/components/aboutUs/WhoWeAre/WhoWeAre.jsx
@@ -1,31 +1,20 @@
 import "./WhoWeAre.css";
+import { useTranslation } from 'react-i18next';
 
 const WhoWeAre = () => {
+    const { t } = useTranslation();
+
     return (
         <section className="who-we-are">
             <div className="image-container">
                 <div className="overlay">
-                    <h2>¿Quiénes somos?</h2>
+                    <h2>{t('whoWeAre.title')}</h2>
                 </div>
             </div>
             <div className="text-container">
-                <p>
-                    <strong>Adventy Travels</strong> es una agencia de viajes 100% poblana, orgullosamente mexicana y legalmente
-                    constituida, <strong>con más de 6 años de experiencia en el sector turístico</strong>. Somos la primera agencia de
-                    viajes registrada legalmente ante la Secretaría de Turismo en <strong>Xicotepec de Juárez</strong>, Puebla, desde
-                    nuestra <strong>fundación el 27 de marzo de 2019</strong>.
-                </p>
-                <p>
-                    Nos destacamos por ofrecer experiencias de viaje personalizadas y de alta calidad, <strong>con un equipo
-                    conformado por jóvenes profesionales comprometidos con la excelencia, la innovación y la
-                    capacitación constante</strong>. A lo largo de nuestra trayectoria, hemos recibido múltiples reconocimientos
-                    y certificaciones en los estados de Puebla e Hidalgo, que nos avalan como una agencia segura,
-                    confiable, responsable y totalmente legal.
-                </p>
-                <p>
-                    Operamos bajo la representación legal de <strong>GRUPO COMERCIAL BRACARDA</strong>, lo que nos permite
-                    ampliar nuestra red de servicios, garantizar el respaldo legal y operativo de nuestra empresa.
-                </p>
+                <p dangerouslySetInnerHTML={{ __html: t('whoWeAre.description1') }}></p>
+                <p dangerouslySetInnerHTML={{ __html: t('whoWeAre.description2') }}></p>
+                <p dangerouslySetInnerHTML={{ __html: t('whoWeAre.description3') }}></p>
             </div>
         </section>
     );
