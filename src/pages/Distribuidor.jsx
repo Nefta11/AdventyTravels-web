70db893f4ca4998ceedd8cd95b897b1c8c432a9b
Neftali Vergara <neftaliarturohernandez@gmail.com>
2025-04-12 14:06:05 -0600

    Refactor AdventyDistribuidorasHeader; move component and styles to new directory structure

--- src/pages/Distribuidor.jsx
+++ src/pages/Distribuidor.jsx
@@ -2,7 +2,7 @@ import { motion } from 'framer-motion';
 import NavComponent from '../components/generals/nav/navComponent';
 import FooterComponent from '../components/generals/footer/FooterComponent';
 import WhatsAppIcon from "../components/generals/whatsAppComponent/WhatsAppIcon";
-import AdventyDistribuidorasHeader from '../components/distribuidora/AdventyDistribuidorasHeader';
+import AdventyDistribuidorasHeader from '../components/distribuidora/header/AdventyDistribuidorasHeader';
 const Distribuidor = () => {
     return (
         <motion.div
