6b6e653e585f7d0ef73bcc6c1243da1c66013323
Neftali Vergara <neftaliarturohernandez@gmail.com>
2025-04-11 18:11:15 -0600

    Adjust logo image max-width for improved responsiveness; update drop-shadow filter for better visibility

--- src/components/partners/header/PartnerHeader.jsx
+++ src/components/partners/header/PartnerHeader.jsx
@@ -10,7 +10,7 @@ import { useTranslation } from 'react-i18next';
 const CAROUSEL_IMAGES = [
     'https://images.unsplash.com/photo-1573497019236-17f8177b81e8?auto=format&fit=crop&q=80',
     'https://images.unsplash.com/photo-1573497019940-1c28c88b4f3e?auto=format&fit=crop&q=80',
-    'https://images.unsplash.com/photo-1560264280-88b68371db39?auto=format&fit=crop&q=80',
+    'https://images.unsplash.com/photo-1599231091889-c1fd76b87226?q=80&w=1976&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     'https://images.unsplash.com/photo-1573496359142-b8d87734a5a2?auto=format&fit=crop&q=80',
     'https://images.unsplash.com/photo-1557425493-6f90ae4659fc?auto=format&fit=crop&q=80',
     'https://images.unsplash.com/photo-1573497491765-dccce02b29df?auto=format&fit=crop&q=80'
