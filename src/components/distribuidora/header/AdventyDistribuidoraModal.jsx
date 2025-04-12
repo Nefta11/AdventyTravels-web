9832942de54601e8460fa992d6d61835d613e6a9
Neftali Vergara <neftaliarturohernandez@gmail.com>
2025-04-12 14:15:51 -0600

    Enhance AdventyDistribuidoraModal input validation; add maxLength and pattern attributes for improved user input handling

--- src/components/distribuidora/header/AdventyDistribuidoraModal.jsx
+++ src/components/distribuidora/header/AdventyDistribuidoraModal.jsx
@@ -16,27 +16,36 @@ const AdventyDistribuidoraModal = ({ isOpen, onClose }) => {
                         type="text"
                         className="adventy-modal-input"
                         placeholder="Nombre"
+                        maxLength="70"
                     />
                     <input
                         type="email"
                         className="adventy-modal-input"
                         placeholder="Email"
+                        maxLength="40"
+                        pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+                        title="Por favor, ingrese un correo electrónico válido."
                     />
                     <input
                         type="tel"
                         className="adventy-modal-input"
                         placeholder="Whatsapp"
+                        maxLength="12"
+                        pattern="\d{1,12}"
+                        title="Por favor, ingrese hasta 12 caracteres numéricos."
                     />
                     <div className="adventy-modal-row">
                         <input
                             type="text"
                             className="adventy-modal-input"
                             placeholder="País"
+                            maxLength="50"
                         />
                         <input
                             type="text"
                             className="adventy-modal-input"
                             placeholder="Ciudad"
+                            maxLength="50"
                         />
                     </div>
                     <button type="submit" className="adventy-modal-submit">
