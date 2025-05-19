f4ea7624eb0e0d03959e3b820c63696f885f0bd3
Neftali Vergara <neftaliarturohernandez@gmail.com>
2025-05-19 10:07:02 -0600

    Remove hardcoded image URL in AdventyDistributorSection component for improved flexibility

--- src/components/distribuidora/adventyDis/AdventyDistributorSection.jsx
+++ src/components/distribuidora/adventyDis/AdventyDistributorSection.jsx
@@ -87,7 +87,7 @@ const AdventyDistributorSection = () => {
                         <div className="neft-main-image">
                             <div className="neft-image-overlay-filter"></div>
                             <img
-                                src="https://images.unsplash.com/photo-1607021836603-4c7f826a7a88?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                                src=""
                                 alt="Mujer emprendedora con laptop"
                                 className="neft-image-primary"
                             />
