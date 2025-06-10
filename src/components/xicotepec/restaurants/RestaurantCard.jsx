0c139a6f4294d9f26c162906181ad9d423705bd5
Neftali Vergara <neftaliarturohernandez@gmail.com>
2025-06-10 16:19:34 -0600

    feat: Remove default image fallback in RestaurantCard component

--- src/components/xicotepec/restaurants/RestaurantCard.jsx
+++ src/components/xicotepec/restaurants/RestaurantCard.jsx
@@ -14,7 +14,7 @@ const RestaurantCard = ({ restaurant, onClick }) => {
                     alt={restaurant.name}
                     className="restaurant-image"
                     onError={(e) => {
-                        e.target.src = 'https://images.unsplash.com/photo-1555396273-367ea4eb4db5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80';
+                        e.target.src = '';
                     }}
                 />
                 <div className="restaurant-category-badge">
